324dea7bb653f99866a56952ced3a71313aac888
NicoRathmayr <93470147+NicoRathmayr@users.noreply.github.com>
2024-02-21 09:55:29 +0100

    Update tt_um_digitaler_filter_rathmayr.v

--- src/tt_um_digitaler_filter_rathmayr.v
+++ src/tt_um_digitaler_filter_rathmayr.v
@@ -16,7 +16,7 @@
 `timescale 1ns/1ps
 
 
-module tt_um_digitaler_filter(
+module tt_um_digitaler_filter_rathmayr(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
